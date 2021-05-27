5302b29c5fa2daddcc96ae5525bf19812aa02e9c
qiangzhang <qiangzhang0925@126.com>
2021-05-27 10:08:59 +0800

    change data.json

--- app/authorization.py
+++ app/authorization.py
@@ -1,8 +1,8 @@
 # Get an Key and Secret
 # https://www.binance.com/restapipub.html
 
-api_key = ''
-api_secret = ''
+api_key = '097e079c-97a8-417b-81da-272b8c97c668'
+api_secret = 'B28472518D1A7C591A9B3AF3516DE12'
 recv_window = 5000
 
-dingding_token = ""
+dingding_token = "https://oapi.dingtalk.com/robot/send?access_token=9350ab6cd637e6ebafdcf0df14e66cedb424dbe995f85e2a043466a9ab8bb1ab"
